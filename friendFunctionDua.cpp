dfb6e8b09610891775e597c28391dd3570e0576d
fairus0305 <ruszezzy@gmail.com>
2026-04-27 18:51:08 +0700

    membuat class pelajar dengan atribut private nilai dan constructor default

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -7,3 +7,13 @@ class manusia
 public:
     void showNilaipelajar(pelajar &x);
 };
+
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaipelajar(pelajar &x);
+};
