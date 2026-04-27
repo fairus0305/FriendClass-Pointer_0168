f5a72981a32acf976868a22566ce7c36557d55d6
fairus0305 <ruszezzy@gmail.com>
2026-04-27 19:05:54 +0700

    merapikan codee

--- tugas.cpp
+++ tugas.cpp
@@ -15,13 +15,14 @@ public:
     }
     void input()
     {
-        cout << "Masukkan diagonal 1  : " << endl;
+        cout << "==input Layang layang==" << endl;
+        cout << "Masukkan diagonal 1  : " ;
         cin >> d1;
-        cout << "Masukkan diagonal 2  : " << endl;
+        cout << "Masukkan diagonal 2  : " ;
         cin >> d2;
-        cout << "Masukkan sisi 1  : " << endl;
+        cout << "Masukkan sisi 1  : " ;
         cin >> s1;
-        cout << "Masukkan sisi 2  : " << endl;
+        cout << "Masukkan sisi 2  : " ;
         cin >> s2;
     }
     friend void tampilkanKeliling(LayangLayang objLayang, BelahKetupat objBelah);
@@ -39,11 +40,12 @@ public:
     }
     void input()
     {
-        cout << "Masukkan diagonal 1  : " << endl;
+        cout << "======input Ketupat====" << endl;
+        cout << "Masukkan diagonal 1  : ";
         cin >> d1;
-        cout << "Masukkan diagonal 2  : " << endl;
+        cout << "Masukkan diagonal 2  : ";
         cin >> d2;
-        cout << "Masukkan sisi        : " << endl;
+        cout << "Masukkan sisi        : ";
         cin >> sisi;
     }
     friend void tampilkanKeliling(LayangLayang objLayang, BelahKetupat objBelah);
